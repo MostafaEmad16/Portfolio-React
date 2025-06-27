6ee089749e46fa98586b25e62198ab537ab1e93c
Mostafa Emad <151018214+MostafaEmad16@users.noreply.github.com>
2025-06-27 08:41:14 +0300

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -42,10 +42,18 @@ function About() {
           </div>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
-            qui quia nulla fugiat expedita adipisci mollitia nihil, unde
-            exercitationem aperiam dolore? Temporibus error beatae inventore
-            assumenda aliquid quod mollitia obcaecati?
+            I am Mostafa Emad, a Full Stack Software Engineer and Web Developer with real-world experience in implementing numerous projects.
+            I am currently working on a web project for the global NASA Space Apps competition.
+            I have been recognized in numerous teams and initiatives, and have participated in courses at academies such as Tech Stars.
+            I also have significant achievements in the field of creativity and robotics, including:
+
+🏆 First place in the ISF competition at the district and governorate levels
+
+🥇 First place in the ISFE competition at the district and governorate levels
+
+🤖 First place in the Robotics Olympics at the district and Giza levels, and qualified for the national finals
+
+Among the achievements I am most proud of is being part of the team that submitted a project to join Japan, and contributing to the establishment of the Japanese Ministry of Technology, in the presence of Dr. Amr Basila, His Excellency the Egyptian Minister of Education, Dr. Mohamed Ahmed Abdel Latif, and businessman Emad El-Suwaidi.
           </p>
           <a href="#conotact" className="btn btn-primary">let's Tolk</a>
         </div>
